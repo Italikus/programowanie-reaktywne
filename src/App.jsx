1ef5a43eba5c629698d3e93e376db5b60e08d7a8
Italikus <132671208+Italikus@users.noreply.github.com>
2023-05-05 14:54:39 +0200

    lab07

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./counter/counter";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
